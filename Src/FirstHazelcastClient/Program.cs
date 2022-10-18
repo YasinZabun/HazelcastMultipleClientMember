9421eb2ea897ab39bbfba7c422ea55fc3f08d895
Yasin Zabun <yasin.zabun@detaysoft.com>
2022-10-18 17:45:03 +0300

    multiple hazelcast clients and members

---
+++ Src/FirstHazelcastClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Hazelcast;
+
+Console.WriteLine("I'm first hazelcast client");
+
+var options = new HazelcastOptionsBuilder().Build();
+options.ClientName = "first-hazelcast-client";
+options.ClusterName = "hello-world";
+options.Networking.SmartRouting = false;
+var client = HazelcastClientFactory.StartNewClientAsync(options).Result;
+
+var map = await client.GetMapAsync<string, string>("my-distributed-map");
+
+
+await map.PutAsync("1", "John");
+await map.PutAsync("2", "Mary");
+await map.PutAsync("3", "Jane");
+
+Console.WriteLine("hazelcast first client finished");
+Console.ReadLine();
---
+++ Src/SecondHazelcastClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Hazelcast;
+
+await Task.Delay(5000);
+Console.WriteLine("I'm second hazelcast client");
+
+var options = new HazelcastOptionsBuilder().Build();
+options.ClientName = "second-hazelcast-client";
+options.ClusterName = "hello-world";
+options.Networking.SmartRouting = false;
+var client = await HazelcastClientFactory.StartNewClientAsync(options);
+
+var map = await client.GetMapAsync<string, string>("my-distributed-map");
+
+var firstElementFromMap = await map.GetAsync("1");
+
+Console.WriteLine(firstElementFromMap);
+
+Console.WriteLine("hazelcast second client finished");
+Console.ReadLine();
